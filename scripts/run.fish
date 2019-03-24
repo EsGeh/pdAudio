d4ba7c94abb67b1c59f29d1b280cdf703a1e8649
EsGeh <SamuelGfroerer@googlemail.com>
2019-03-24 16:36:45 +0100

    improved run script

--- scripts/run.fish
+++ scripts/run.fish
@@ -1,9 +1,62 @@
 #!/bin/fish
 
 set BASE_DIR (dirname (readlink -m (status filename)))/..
-set DEP_DIR $BASE_DIR/dependencies
+set SCRIPTS_DIR (dirname (readlink -m (status filename)))
+
+source "$SCRIPTS_DIR/utils/cmd_args.fish"
+
+
+#################################################
+# variables
+#################################################
 
 set install_dest "$BASE_DIR/install"
+
+set patch "audio_doc.pd"
+
+# (syntax: short/long/description)
+set options_descr \
+	"h/help/print help"
+
+#################################################
+# functions
+#################################################
+
+function print_help
+	echo "usage: "(status -f)" [OPTIONS...] [PATCH]"
+	echo
+	echo "ARGUMENTS:"
+	echo "  the patch to run. default: '$patch'"
+	echo "OPTIONS:"
+	print_options_descr $options_descr
+end
+
+
+#################################################
+# cmd line interface
+#################################################
+
+set options (options_descr_to_argparse $options_descr)
+
+# parse command line arguments:
+argparse $options -- $argv 2>/dev/null
+or begin
+	print_help
+	exit 1
+end
+if set -q _flag_h
+	print_help
+	exit 0
+else
+end
+
+if test (count $argv) -gt 0
+	set patch $argv
+end
+
+#################################################
+# actual script
+#################################################
 
 pd \
 	-jack \
@@ -11,4 +64,4 @@ pd \
 	-lib zexy \
 	-lib structuredDataC \
 	-lib sdScript \
-	"$install_dest/audio_doc.pd"
+	"$install_dest/$patch"
