c5b40e59eec6866922c099e175acf781d4a58656
EsGeh <SamuelGfroerer@googlemail.com>
2019-03-24 16:35:04 +0100

    updated dependencies

--- dependencies.fish
+++ dependencies.fish
@@ -12,10 +12,10 @@ set dependencies \
 # 	name uri version [initcommand]
 set fishshell_cmd_opts \
 	fishshell-cmd-opts  \
-	git@github.com:EsGeh/fishshell-cmd-opts.git	\
+	https://github.com/EsGeh/fishshell-cmd-opts.git \
 	e446daf1741b416ecb83e4741b4cb7f99645bc11
 set pdUtils \
 	pdUtils \
-	git@github.com:EsGeh/pdUtils.git	\
-	b3d87f37d5782ebf6a00f003068ed6429f44a006 \
+	https://github.com/EsGeh/pdUtils.git \
+	440ac3b078021bab84956411fda9eb459df3d038 \
 	"scripts/init.fish --deps-dir \$DEP_DIR"
