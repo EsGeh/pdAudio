5a59fb17d649c18dbd3b50cec9e2065d5114e692
EsGeh <SamuelGfroerer@googlemail.com>
2019-08-17 21:14:41 +0200

    used dep. improvemnts., additional properties on many objects

--- dependencies.fish
+++ dependencies.fish
@@ -18,10 +18,10 @@ set fishshell_cmd_opts \
 set pdUtils \
 	pdUtils \
 	https://github.com/EsGeh/pdUtils.git \
-	ddbe491d54817882d797ff3d41fe75cb73fb9003 \
+	91a845eb8d8859a19f7bc33d2a2633b950476fd9 \
 	"scripts/init.fish --deps-dir \$DEP_DIR"
 set sgDevice \
 	sgDevice \
 	https://github.com/EsGeh/sgDevice.git \
-	290a31acaf9d923ed61512bb9b6406700e9da77e \
+	9c1f990e699bef84f340adb7e43fc41af130a277 \
 	"scripts/init.fish --deps-dir \$DEP_DIR"
