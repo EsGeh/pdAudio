f67ff9fb12ec6801e9db22bc27fbfeb35bfc2e35
EsGeh <SamuelGfroerer@googlemail.com>
2019-09-06 18:42:50 +0200

    update deps, cleaner and simpler synth and sampler.

--- dependencies.fish
+++ dependencies.fish
@@ -18,10 +18,10 @@ set fishshell_cmd_opts \
 set pdUtils \
 	pdUtils \
 	https://github.com/EsGeh/pdUtils.git \
-	dd5aceac5eca99ae588b99b0ae9155bbfd494d7c \
+	5c5a4351592b5fda152be7d3a6b85fc5275b71b7 \
 	"scripts/init.fish --deps-dir \$DEP_DIR"
 set sgDevice \
 	sgDevice \
 	https://github.com/EsGeh/sgDevice.git \
-	8c68be3c563b7989f04d112a31a0299d13173eb1 \
+	c8c421e8de5703c7a6a4d5d440547c71d92807df \
 	"scripts/init.fish --deps-dir \$DEP_DIR"
